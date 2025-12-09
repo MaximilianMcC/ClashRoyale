95478308b2969fee82172ea803ff3cb5744a7c24
MaximilianMcC <max@maximilian.co.nz>
2025-12-10 11:49:53 +1300

    erhm setup kinda stuff

---
+++ src/Program.cs
@@ -0,0 +1,22 @@
+﻿using Raylib_cs;
+
+class Program
+{
+	public static void Main(string[] args)
+	{
+		Raylib.SetTraceLogLevel(TraceLogLevel.Warning);
+		Raylib.InitWindow(540, 960, "clash ro");
+
+		AssetManager.Textures["debug"] = AssetManager.LoadTexture("./assets/debug.png");
+
+		while (Raylib.WindowShouldClose() == false)
+		{
+			Raylib.BeginDrawing();
+			Raylib.ClearBackground(Color.Green);
+			Raylib.EndDrawing();
+		}
+
+		AssetManager.UnloadEverything();
+		Raylib.EndDrawing();
+	}
+}
